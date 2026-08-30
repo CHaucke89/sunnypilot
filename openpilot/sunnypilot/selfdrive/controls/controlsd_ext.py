eb980ce87d2fbfd5bf1ed1591526dad4280652a1
Christopher Haucke <132518562+CHaucke89@users.noreply.github.com>
2026-08-30 11:31:04 -0400

    get_steer_ratio helper
    Use custom steer ratio logic in controlsd
    Simpler

--- openpilot/sunnypilot/selfdrive/controls/controlsd_ext.py
+++ openpilot/sunnypilot/selfdrive/controls/controlsd_ext.py
@@ -47,6 +47,13 @@ class ControlsExt(ModelStateBase):
     else:
       return lac
 
+  # Determine if we're using the learned steer ratio or a custom fixed value
+  def get_steer_ratio(self, lp):
+    custom_sr = self.params.get("CustomSR")
+    if not self.params.get_bool("UseCustomSR") or custom_sr is None:
+      return max(lp.steerRatio, 0.1)
+    return max(round(custom_sr, 2), 0.1)
+
   def get_params_sp(self, sm: messaging.SubMaster) -> None:
     if time.monotonic() - self._param_update_time > PARAMS_UPDATE_PERIOD:
       self.blinker_pause_lateral.get_params()
